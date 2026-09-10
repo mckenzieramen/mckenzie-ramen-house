4c6be331abeecd85e6740a5167fc258b8a0898e3
mckenzieramen <yahhclffjnd@gmail.com>
2026-09-10 16:41:54 +0800

    Create api.js

---
+++ js/api.js
@@ -0,0 +1,10 @@
+// =========================================
+// McKenzie Ramen House
+// API Connection
+// =========================================
+
+// Google Apps Script API URL will be added here later.
+
+const API_URL = "";
+
+console.log("API module loaded.");
