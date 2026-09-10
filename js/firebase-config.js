363af6f7759ee3f9d8366477b42d0646becd41e5
mckenzieramen <yahhclffjnd@gmail.com>
2026-09-10 18:00:16 +0800

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+window.MCKENZIE_FIREBASE_CONFIG = {TRDXQdqgcNaVgPDfqk8tFVzacqy1}
+  apiKey: "AIzaSyD9L5m9lTbTxN4u8vB6poip5dt94",
+  authDomain: "mckenzie-ramen-house.firebaseapp.com",
+  projectId: "mckenzie-ramen-house",
+  storageBucket: "mckenzie-ramen-house.firebasestorage.app",
+  messagingSenderId: "104828418639",
+  appId: "1:104828418639:web:85e0148036179259c8033a",
+  measurementId: "G-C2KDRE88ZW"
+};
+
+window.MCKENZIE_ADMIN_UID = "PASTE_YOUR_ADMIN_UID_HERE";d
