59f99bdb78a136c9284e258dd3efd22195c20bcd
mckenzieramen <yahhclffjnd@gmail.com>
2026-09-12 15:48:28 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,9 @@
-window.MCKENZIE_FIREBASE_CONFIG = {
+const EMBEDDED_CONFIG = {
   apiKey: "AIzaSyDnLMhAhkAw1JMlbTxN4u8vB6poip5dt94",
   authDomain: "mckenzie-ramen-house.firebaseapp.com",
   projectId: "mckenzie-ramen-house",
   storageBucket: "mckenzie-ramen-house.firebasestorage.app",
   messagingSenderId: "1048288418639",
-  appId: "1:1048288418639:web:85e0148036179259c8033a",
-  measurementId: "G-C2KDRE88ZW"
+  appId: "1:1048288418639:web:9ed8075e210934ffc8033a",
+  measurementId: "G-ERQF1B1W74"
 };
-
-window.MCKENZIE_ADMIN_UID = "OHDs2DV4jyO3eBrww8d0gUQkNli2";
