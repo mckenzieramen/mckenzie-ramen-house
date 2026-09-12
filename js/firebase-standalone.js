d738487a4cd769940c7d5ad4cd2b84ceb928ff70
mckenzieramen <yahhclffjnd@gmail.com>
2026-09-12 16:05:14 +0800

    Update firebase-standalone.js

--- js/firebase-standalone.js
+++ js/firebase-standalone.js
@@ -6785,3 +6785,13 @@ document.addEventListener('DOMContentLoaded',function(){
 </script>
 </body>
 </html>
+
+const EMBEDDED_CONFIG = {
+  apiKey: "AIzaSyDnLMhAhkAw1JMlbTxN4u8vB6poip5dt94",
+  authDomain: "mckenzie-ramen-house.firebaseapp.com",
+  projectId: "mckenzie-ramen-house",
+  storageBucket: "mckenzie-ramen-house.firebasestorage.app",
+  messagingSenderId: "1048288418639",
+  appId: "1:1048288418639:web:9ed8075e210934ffc8033a",
+  measurementId: "G-ERQF1B1W74"
+};
