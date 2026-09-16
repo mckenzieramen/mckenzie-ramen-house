dce1e570a5b4fa5e68ebff63d97ce0fd3c9e3da0
mckenzieramen <yahhclffjnd@gmail.com>
2026-09-16 18:20:26 +0800

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-// =========================================
-// McKenzie Ramen House
-// Main Application JavaScript
-// =========================================
-
-console.log("McKenzie Ramen House website loaded successfully.");
